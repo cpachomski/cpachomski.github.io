853fc3f52f6c4b781b55c339384f82493e9ed053
cpachomski <cpachomski@gmail.com>
2015-03-24 23:26:06 -0400

    add more images to view and compute bcgheight

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1 get window height
+// 2 subtract header height from it
+// 3 set parallax background heights to be that difference
+// 4 set section heighs to be that difference
+
+
+$( document ).ready(function(){
+
+  console.log("we're connected!")
+
+
+  //responsively fit the background heights to the window
+  var windowHeight = $( window ).height();
+  var headerHeight = $( 'header' ).height();
+  var newBcgHeight = windowHeight - headerHeight;
+  console.log(newBcgHeight);
+
+
+
+})
