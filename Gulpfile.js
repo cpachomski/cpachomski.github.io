67dc19f1d4af3464b891ea7a5900a52c037c7288
cpachomski <cpachomski@gmail.com>
2015-09-06 21:31:20 -0400

    setup gulpfile and tasks to watch for sass changes and output a minified css at the end of the pipeline

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+gulp.task('styles', function(){
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'))
+        .pipe(minifyCSS())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', function(){
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
+
