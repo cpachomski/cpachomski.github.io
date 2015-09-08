e53a31aaaaa2308b1b7959cde2c7353e555c39f6
cpachomski <cpachomski@gmail.com>
2015-09-08 19:38:13 -0400

    simplify gulpfile to output unminified css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var rename = require('gulp-rename');
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'))
-        .pipe(minifyCSS())
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./css/'))
 });
 
-gulp.task('default', function(){
-    gulp.watch('sass/**/*.scss', ['styles']);
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
 });
-
