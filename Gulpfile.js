7e4b0fd9472ea194f147ce0326e1e9e3b8d53a79
cpachomski <cpachomski@gmail.com>
2015-12-17 23:47:04 -0500

    create new gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,34 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var rename = require('gulp-rename')
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var bourbon = require('node-bourbon');
+var minifycss = require('gulp-minify-css');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
+
+var paths = {
+  scripts: [],
+  styles: ['./css/scss/**/*.scss'],
+  stylesDEST: './css',
+  scriptsDEST: './js/dist'
+}
+
+
+gulp.task('watch', function(){
+  gulp.watch(paths.styles, ['styles']);
+});
 
 gulp.task('styles', function(){
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'))
-        .pipe(minifyCSS())
-        .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('./'));
+  gulp.src(paths.styles)
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      includePaths: require('node-bourbon').includePaths
+    }))
+    .pipe(concat('style.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest(paths.stylesDEST))
+    .pipe(livereload({start: true}));
 });
-
-gulp.task('default', function(){
-    gulp.watch('sass/**/*.scss', ['styles']);
-});
-
